f3b38c8a2a5de4e5e77b44c4f520c059edefc0ab
Erki Tõnuri <erki.tonuri@gmail.com>
2016-04-27 16:04:17 +0300

    Lisas palju kommentaare, mis mingi asi teeb

--- tripstrapstrull/tripstrapstrull/Main.cpp
+++ tripstrapstrull/tripstrapstrull/Main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
 
+// loob mängulaua ja nummerdab kõik kohad ära
 char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+// loob mängija
 char player = 'X';
 
 void Draw()
+// Joonistab mängulaua konsooli
 {
 	for (int i = 0; i < 3; i++)
 	{
@@ -18,6 +21,7 @@ void Draw()
 
 void Input()
 {
+	// inimene sisestab numbri kuhu tahab käia
 	int a;
 	cout << "Vali millise kohta tahad käia: ";
 	cin >> a;
@@ -44,6 +48,7 @@ void Input()
 
 void TogglePlayer()
 {
+	// vahetab mängia ära peale igat käiku
 	if (player == 'X')
 		player = 'O';
 	else
@@ -53,6 +58,7 @@ void TogglePlayer()
 char Win()
 {
 	// kontrollib kas 1 mängija võibab 
+	// kontrollin ridu
 	if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X')
 		return 'X';
 	if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X')
@@ -60,6 +66,7 @@ char Win()
 	if (matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X')
 		return 'X';
 
+	//kontrollib veerge
 	if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X')
 		return 'X';
 	if (matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X')
@@ -67,6 +74,7 @@ char Win()
 	if (matrix[0][2] == 'X' && matrix[1][2] == 'X' && matrix[2][2] == 'X')
 		return 'X';
 
+	//kontrollib diagonaale
 	if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X')
 		return 'X';
 	if (matrix[2][0] == 'X' && matrix[1][1] == 'X' && matrix[0][2] == 'X')
@@ -74,6 +82,8 @@ char Win()
 
 	// kontrollib kas teine mängija võibab
 	// vahetas ümber Notepad++, et kiiremini teha
+
+	// kontrollin ridu
 	if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O')
 		return 'O';
 	if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O')
@@ -81,6 +91,7 @@ char Win()
 	if (matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O')
 		return 'O';
 
+	//kontrollib veerge
 	if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O')
 		return 'O';
 	if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O')
@@ -88,16 +99,21 @@ char Win()
 	if (matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O')
 		return 'O';
 
+	//kontrollib diagonaale
 	if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O')
 		return 'O';
 	if (matrix[2][0] == 'O' && matrix[1][1] == 'O' && matrix[0][2] == 'O')
 		return 'O';
+
+	//tagastab midagi muidu paneb programm metsa
 	return '/';
 }
 
 int main()
 {
+	// kutsub joonistamise välja
 	Draw();
+	// loob lõpmatu korduse
 	while (1)
 	{
 		Input();
@@ -114,6 +130,8 @@ int main()
 		}
 		TogglePlayer();
 	}
+	// süsteemi paus enne kui paneb programmi kinni
 	system("pause");
+	// sulgeb programmi
 	return 0;
 }
